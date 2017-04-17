d04fc05ac85807aa22442eba075c1b8f79b2cda7
nwl1515 <nw.leth@gmail.com>
2017-04-17 21:16:56 +0200

    Added c++11 flag and initial method for roadmap

--- include/Roadmap.hpp
+++ include/Roadmap.hpp
@@ -6,6 +6,20 @@
 #include <rw/kinematics/State.hpp>
 #include <rw/models/Device.hpp>
 #include <rw/rw.hpp>
+#include <rw/math/Q.hpp>
+
+class Node
+{
+ 	Node(rw::math::Q p){ q_val = p; edges = std::vector<Node*>(0);}
+	~Node();
+	 
+	void add_edge(Node* e){ edges.push_back(e);}
+
+	rw::math::Q q_val;
+	std::vector<Node*> edges;
+        bool connected = false;
+};
+
 
 class Roadmap
 {
@@ -14,13 +28,30 @@ public:
 	Roadmap(); 
 
 	virtual ~Roadmap();
-	void initWorkCell();
+
+        void create_roadmap(int size, double resolution, double connection_radius);
+        void save_roadmap(std::string path);
+	void load_roadmap(std::string path);
+
+	
+	// ROS topics	
+
+	// We probably need to implement the graph search in here, givin the transfer time for ros topics will slow it down
+	void find_path(rw::math::Q init, rw::math::Q goal);
+
+	// If the distance is small enough, do not start a graph search, insted make graping using RRT (seperate ROS topic!)
+	void distance_to_goal(rw::math::Q init, rw::math::Q goal);
+	 
  
 
 
 
 
 protected:
+	void initWorkCell();
+
+	// Find connection node for init and goal from a Q position
+	Node* find_connection_node(rw::math::Q);
 
 
 
@@ -31,6 +62,7 @@ public:
   	rw::models::WorkCell::Ptr _workcell;
 	rw::kinematics::State _state;
   	rw::models::Device::Ptr _device;
+        std::vector<Node*> *_graph;
 
 };
 
