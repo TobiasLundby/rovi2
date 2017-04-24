45cc0e48bfbee8b82b823b23a67ac8f97b6f75f1
Tobias Lundby <tobiaslundby@gmail.com>
2017-04-24 08:12:25 +0200

    Sync of frames now working

--- include/ColorDetector.hpp
+++ include/ColorDetector.hpp
@@ -7,6 +7,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <cmath>
+#include <string>
 
 // Namespaces
 using namespace cv;
@@ -26,20 +27,23 @@ class ColorDetector {
 public:
   ColorDetector();
   std::vector<Point2f> FindMarker(Mat &image);
+  void set_result_window_name(std::string name);
 private:
-  int erode_color_iterations  = 1;
-  int dilate_color_iterations = 3;
+    std::string window_name = "Result 2D";
 
-  SimpleBlobDetector::Params params;
-  Ptr<SimpleBlobDetector> detector;
+    int erode_color_iterations  = 1;
+    int dilate_color_iterations = 3;
 
-  // *** MARKER RED / RED BALL *** (the red is shifted into the yellow area)
-  int hsv_h_base_MR        = 55; // 55 is almost green
-  int hsv_h_sensitivity_MR = 9; // 5 for big green led and 24 for most other
-  int hsv_h_low_MR         = hsv_h_base_MR - hsv_h_sensitivity_MR; //hsv_h_base - hsv_h_sensitivity;
-  int hsv_h_upper_MR       = hsv_h_base_MR + hsv_h_sensitivity_MR;//hsv_h_base + hsv_h_sensitivity;
-  int hsv_s_low_MR         = 135; //100;
-  int hsv_s_upper_MR       = 255;
-  int hsv_v_low_MR         = 98; //100;
-  int hsv_v_upper_MR       = 255;
+    SimpleBlobDetector::Params params;
+    Ptr<SimpleBlobDetector> detector;
+
+    // *** MARKER RED / RED BALL *** (the red is shifted into the yellow area)
+    int hsv_h_base_MR        = 55; // 55 is almost green
+    int hsv_h_sensitivity_MR = 9; // 5 for big green led and 24 for most other
+    int hsv_h_low_MR         = hsv_h_base_MR - hsv_h_sensitivity_MR; //hsv_h_base - hsv_h_sensitivity;
+    int hsv_h_upper_MR       = hsv_h_base_MR + hsv_h_sensitivity_MR;//hsv_h_base + hsv_h_sensitivity;
+    int hsv_s_low_MR         = 135; //100;
+    int hsv_s_upper_MR       = 255;
+    int hsv_v_low_MR         = 98; //100;
+    int hsv_v_upper_MR       = 255;
 };
