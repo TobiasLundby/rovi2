75c49f604306a68f902f95e6c5dba2ee5c86f30e
Mathias Hoejgaard Egeberg <mhoejgaard@hotmail.com>
2017-05-09 17:21:30 +0200

    Converted camera frame coordinates to robot coordinates.

--- include/vision_test.hpp
+++ include/vision_test.hpp
@@ -4,6 +4,7 @@
 #include "ros/ros.h"
 #include "rovi2/State.h"
 #include "rovi2/position3D.h"
+#include "rovi2/Q.h"
 
 // Robwork
 #include <rw/rw.hpp>
@@ -31,25 +32,34 @@ public:
   ~vision_test();
 
 private:
+  // NodeHandle
   ros::NodeHandle nh;
 
+  // Subscribers
   ros::Subscriber subscribe_robot_status;
   ros::Subscriber subscribe_triangulated;
   ros::Subscriber subscribe_estimated;
   ros::Subscriber subscribe_predicted;
 
+  // Subscriber callbacks
   void robot_status_callback(const rovi2::State &msg);
   void triangulated_callback(const rovi2::position3D &msg);
   void estimated_callback(const rovi2::position3D &msg);
   void predicted_callback(const rovi2::position3D &msg);
 
+  // Attributes
   rovi2::State robot_state;
   rovi2::position3D estimated_pos;
   rovi2::position3D triangulated_pos;
 
+  // Robwork stuff
   rw::models::WorkCell::Ptr _workcell;
   rw::models::Device::Ptr _device;
   rw::kinematics::State _state;
+  rw::kinematics::Frame* robot_base;
+  rw::kinematics::Frame* TCP_marker;
+
+  rw::math::Q toRw(const rovi2::Q& q);
 
 
 };
